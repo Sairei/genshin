fb16c5eb60e36fb12f0d700d9cb590746a269504
christopher.cauet <christopher.cauet@sully-group.fr>
2022-02-18 00:27:00 +0100

    :hammer: Ajout du portrait dans la page info

--- src/component/page/character/CharacterPage.js
+++ src/component/page/character/CharacterPage.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { useParams } from 'react-router-dom';
+import { Container, Image } from '@mantine/core';
+import CharacterInfoPage from './info/CharacterInfoPage';
+
+const genshindb = require('genshin-db');
 
 const CharacterPage = () => {
   const { name } = useParams()
+  const [characterData, setData] = useState();
+
+  useEffect(() => {
+    setData(genshindb.characters(name, { resultLanguage: "French" }));
+  }, [name])
+
+  console.log(characterData);
 
   return (
-    <div>
-      {name}
+    <div className='character_main_container'>
+      <Container className='character_container'>
+        <CharacterInfoPage />
+
+        <Image
+          className='charac_info_image'
+          src={characterData.images.cover2}
+          fit='contain'
+        />
+      </Container>
     </div>
   );
 };
---
+++ src/component/page/character/info/CharacterInfoPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterInfoPage = () => {
+  return (
+    <div className='charac_info_container'>
+      
+    </div>
+  );
+};
+
+export default CharacterInfoPage;
