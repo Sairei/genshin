2258aed8f1f0b1a2af759249eddb0d75fdcb8ae6
christopher.cauet <christopher.cauet@sully-group.fr>
2022-02-27 01:05:31 +0100

    :hammer: Ajout de la fonctionnalité de filtre dans la liste des personnages

--- src/component/page/characters/ListCharactersPage.js
+++ src/component/page/characters/ListCharactersPage.js
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
+import { useSelector } from 'react-redux';
 import { Space } from '@mantine/core';
 
-import CharacterCard from './CharacterCard';
 import { simplifyText } from '../../../utils/converter/simplifyElementText';
+import CharacterCard from './CharacterCard';
 import SortElement from './SortElement';
 
 const genshindb = require('genshin-db');
 
 const ListCharactersPage = () => {
   const [caracterList, setList] = useState([]);
-
+  const [filterList, setFilterList] = useState([]);
+  
   useEffect(() => {
     let list = [];
     genshindb.characters('name', { matchCategories: true }).forEach(name => {
       let c = genshindb.characters(name);
-
+      
       let obj = {};
       obj['name'] = name;
       obj['elt'] = simplifyText(c.element !== "None" ? c.element : 'adaptive');
@@ -23,13 +25,48 @@ const ListCharactersPage = () => {
       obj['region'] = simplifyText(c.region);
       obj['rarity'] = simplifyText(c.rarity);
       obj['gender'] = simplifyText(c.gender);
-
+      
       list.push(obj);
     });
     setList(list);
+    setFilterList(list);
     console.log(list);
   }, [])
+  
+  const elementValue = useSelector((state) => state.element);
+  const weaponValue = useSelector((state) => state.weapon);
+  const regionValue = useSelector((state) => state.region);
+  const genderValue = useSelector((state) => state.gender);
+  useEffect(() => {
+    let l = caracterList;
 
+    if (elementValue) {
+      l = l.filter((e) => {
+        return e.elt === elementValue;
+      })
+    }
+
+    if (weaponValue) {
+      l = l.filter((e) => {
+        return e.weapon === weaponValue;
+      })
+    }
+
+    if (regionValue) {
+      l = l.filter((e) => {
+        return e.region === regionValue;
+      })
+    }
+
+    if (genderValue) {
+      l = l.filter((e) => {
+        return e.gender === genderValue;
+      })
+    }
+
+    setFilterList(l);
+  }, [caracterList, elementValue, weaponValue, regionValue, genderValue]);
+  
   return (
     <div className='character_list_container'>
       <SortElement />
@@ -39,7 +76,7 @@ const ListCharactersPage = () => {
       <div className='list_characters'>
         <ul>
           {
-            caracterList.map(character => {
+            filterList.map(character => {
               let e = character.name
               return (
                 <li key={e}>
