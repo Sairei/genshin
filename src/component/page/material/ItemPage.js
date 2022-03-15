e0c35956cdffaa7daa3d241916c322e7f1508473
christopher.cauet <christopher.cauet@sully-group.fr>
2022-03-15 18:14:01 +0100

    :sparkles: Debut de la page des items

--- src/component/page/material/ItemPage.js
+++ src/component/page/material/ItemPage.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
+import { Image, Title } from '@mantine/core';
 import { useParams } from 'react-router-dom';
+
+import { findImage } from '../../../utils/finder/findImage';
+import { convertInfo } from '../../../utils/converter/convertInfoToHTML';
 
 const genshindb = require('genshin-db');
 
 const ItemPage = () => {
   const { name } = useParams()
-  const data = genshindb.materials(name);
+  const data = genshindb.materials(name, { resultLanguage: "French" });
+
+  console.log(data);
 
   return (
     <div className='item_container'>
-      
+      <div className='name_image'>
+        <Image fit='contain'
+          width={100} height={100}
+          src={findImage(data.images.nameicon)} />
+
+        <div className='name vertical_align_text'>
+          <Title order={1}>
+            {data.name}
+          </Title>
+        </div>
+      </div>
+
+      <div className='item_description'>
+        {convertInfo(data.description)}
+      </div>
+
+      <div className='item_sources'>
+        {
+          data.source.map((s, index) => {
+            return (
+              <div key={index}>
+                {s}
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 };
