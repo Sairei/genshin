cda0ec8110123f81ebcadf6834010c9c0c09da4b
christopher.cauet <christopher.cauet@sully-group.fr>
2022-03-23 10:11:05 +0100

    :hammer: Ajout des liens dans la page d'equipement

--- src/component/page/equipment/EquipmentPage.js
+++ src/component/page/equipment/EquipmentPage.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const EquipmentPage = () => {
   return (
     <div className='equipment_main_container'>
-      
+      <Link to='/equipment/weapon' >
+        <div>
+          Armes
+        </div>
+      </Link>
+
+      <div className='vertical_separator'></div>
+
+      <Link to='/equipment/artifact' >
+        <div>
+          Artefact
+        </div>
+      </Link>
     </div>
   );
 };
