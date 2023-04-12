6468bc4786de489c906be023bc312b453c4d2fe9
Sairei <christophercauet@hotmail.fr>
2023-04-12 09:57:26 +0200

    :wrench: Succès ajouté au repo principal

--- src/router/provider/GenshinProvider.js
+++ src/router/provider/GenshinProvider.js
@@ -1,15 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { Provider } from 'react-redux';
 
 import { store } from './Store';
-import { InitDB } from '../../utils/database/genshinbd';
 
 const GenshinProdiver = ({ children }) => {
-  useEffect(() => {
-    InitDB();
-  }, [])
-  
   return (
     <Provider store={store} >
       {children}
